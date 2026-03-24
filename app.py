8dfa56a423cdaa2f8e2d5c2dd7df44041d06ee5a
vvalfridsson <viggeswe220@gmail.com>
2026-03-24 14:44:02 +0100

    Menu: add `substract`choice promt

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 def run():
+<<<<<<< Updated upstream
     print("1) Subtract\n0) Exit")
     choice = input("Your choice: ")
+=======
+    print("1) Subtract\n0)Exit")
+    choice = input("Your choice")
+    
+>>>>>>> Stashed changes
