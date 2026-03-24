eef5105ca839c90262011232b2d78d0a6246003b
dankodinh <dannydinhs1@gmail.com>
2026-03-24 15:10:37 +0100

    Call add(a, b) and print result

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ from calculator import add
 def run():
     print("1) Subtract\n0)Exit")
     choice = input("Select")
-    
+    a = float(input("a: "))    
+    b = float(input("b: "))
+    print(add(a, b))
