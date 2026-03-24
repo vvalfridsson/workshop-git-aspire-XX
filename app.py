935573da6511553899ffb780033d9a5ad3b15840
vvalfridsson <viggeswe220@gmail.com>
2026-03-24 14:51:15 +0100

    add calculator module with add() and import

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
+from calculator import add
 def run():
-<<<<<<< Updated upstream
-    print("1) Subtract\n0) Exit")
-    choice = input("Your choice: ")
-=======
     print("1) Subtract\n0)Exit")
     choice = input("Your choice")
     
->>>>>>> Stashed changes
