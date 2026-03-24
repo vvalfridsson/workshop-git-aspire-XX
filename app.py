f41fa1ba9d20e1164e13f601631fc70ef4a4c71e
dankodinh <dannydinhs1@gmail.com>
2026-03-24 15:24:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from calculator import add, sub, mul
+from calculator import add
 def run():
     print("1) Subtract\n0)Exit")
     choice = input("Select")
     a = float(input("a: "))    
     b = float(input("b: "))
     print(add(a, b))
-    if choice == "2": print(sub(a, b))
-    if choice == "3": print(mul(a, b))
