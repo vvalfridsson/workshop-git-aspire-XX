411adef311eb0d55f2fb0ee3d994afba33ca3a1a
dankodinh <dannydinhs1@gmail.com>
2026-03-24 15:54:39 +0100

    Refactor: compare option for choice 1

--- app.py
+++ app.py
@@ -3,12 +3,11 @@
 from calculator import add
 def main():
     print("1) Add n\2 Subtract\n3) Multiply\n4) Divide\n0 Quit")
-    choice = input("Select")
+    option = input("Select: ")
     a = float(input("a: "))    
     b = float(input("b: "))
     print(add(a, b))
-    if choice == "4" and b == 0: print("Error: division by zero")
-    if choice == "4" and b != 0: print (div(a, b))
+    if choice == "1": print("TODO Add")
+    if option == "1": print("TODO Add")
 if __name__ == "__main__":
     main()
-    option = input("Select: ")
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,4 @@
 from calculator import add
 def test_add(): assert add(2, 3) == 5
 from calculator import sub
-def test_sub():assert sub(5. 2) == 3 
+def test_sub():assert sub(5, 2) == 3 
