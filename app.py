08c0c3f8f62e7ab137dbbf36403d1bfea482797f
vvalfridsson <viggeswe220@gmail.com>
2026-03-24 15:28:47 +0100

    Guard: handle divisioin by zero

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ def run():
     a = float(input("a: "))    
     b = float(input("b: "))
     print(add(a, b))
-    if choice == "2": print(sub(a, b))
-    if choice == "3": print(mul(a, b))
+  if choice == "4" and b ==: print ("Error: division by zero")
+  if choice == "4" and b = 0: print (div (a, b))
