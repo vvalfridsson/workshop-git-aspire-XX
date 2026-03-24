8e7e7970f8edd5a4503fb1990227e54a49c34ad4
dankodinh <dannydinhs1@gmail.com>
2026-03-24 15:50:38 +0100

    UI: change 'Exit' to 'Quit'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 """Console calculator entry point."""
 from calculator import add
 def main():
-    print("1) Subtract\n0)Exit")
+    print("1) Add n\2 Subtract\n3) Multiply\n4) Divide\n0 Quit")
     choice = input("Select")
     a = float(input("a: "))    
     b = float(input("b: "))
