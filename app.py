ae64fd204de35c027735ca3d59ef89636993a6b0
dankodinh <dannydinhs1@gmail.com>
2026-03-24 16:11:57 +0100

    Feat: print message on unknown option

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-ERR_DIV_ZERO = "Error: division by zero"
 #simple console calculator
 """Console calculator entry point."""
 from calculator import add
@@ -11,6 +10,8 @@ def main():
     b = float(input("b: "))
     print(add(a, b))
     if option == "3": print(mul(a, b))
-    if option == "4" and b == 0: print("ERR_DIV_ZERO")
+    if option == "4" and b == 0: print("Error: division by zero")
 if __name__ == "__main__":
     main()
+else:
+    print("Unknown option")
